7524064bc67f956270981e30f567e18654321818
Bibbeep <habibalfrb@gmail.com>
2026-05-02 03:14:30 +0700

    added solution for LeetCode's Group Anagrams

---
+++ leetcode/solutions/49-group-anagrams.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <cstddef>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    vector<vector<string>> ans;
+    unordered_map<string, vector<string>> dict;
+
+    for (auto str : strs) {
+        string val = str;
+        sort(str.begin(), str.end());
+        dict[str].push_back(val);
+    }
+
+    for (auto it : dict) {
+        vector<string> grp;
+        for (auto s : it.second) {
+            grp.push_back(s);
+        }
+
+        ans.push_back(grp);
+    }
+
+    return ans;
+}
+
+int main() {
+    size_t size;
+    cin >> size;
+    vector<string> strs;
+
+    string s;
+    while (size--) {
+        cin >> s;
+        strs.push_back(s);
+    }
+
+    vector<vector<string>> result = groupAnagrams(strs);
+
+    cout << "[";
+    for (vector<string> res : result) {
+        cout << "[";
+        for (string str : res) {
+            cout << str << ",";
+        }
+        cout << "],";
+    }
+    cout << "]\n";
+
+    return 0;
+}
